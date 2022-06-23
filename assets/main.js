2bc410d79a80e21df590f78366b797e6394c841d
Florence Nafula <flowanaswa@gmail.com>
2022-06-23 22:50:46 +0300

    Change in JS

--- assets/main.js
+++ assets/main.js
@@ -1,5 +1,8 @@
 const button5 = document.querySelector("#plusFive")
-const button10 = document.querySelector("#plusFive")
+const button10 = document.querySelector("#plusTen")
+const button15 = document.querySelector("#plusFifteen")
+const button20 = document.querySelector("#plusTwenty")
+
 const indicator = document.querySelector("#number-indicator")
 const disp = document.querySelector("#display-heading")
 
@@ -13,3 +16,12 @@ button10.addEventListener('click', () => {
   indicator.textContent = (parseInt(indicator.textContent) + 10).toString()
 })
 
+button15.addEventListener('click', () => {
+    disp.textContent = "added 15"
+    indicator.textContent = (parseInt(indicator.textContent) + 15).toString()
+  })
+
+  button20.addEventListener('click', () => {
+    disp.textContent = "added 20"
+    indicator.textContent = (parseInt(indicator.textContent) + 20).toString()
+  })
